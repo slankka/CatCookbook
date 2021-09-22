10011aca8ebd788b58d63891a34a6d52b79d07d0
slankka <slankka@users.noreply.github.com>
2021-09-22 14:19:44 +0800

    Update Layout.vue
    add Layout

--- docs/.vuepress/theme/layouts/Layout.vue
+++ docs/.vuepress/theme/layouts/Layout.vue
@@ -1,28 +1,21 @@
 <template>
   <ParentLayout>
-    <template #page-bottom>
-      <footer class="footer">
-        Site made by © 2021 😀 <a href="https://slankka.com" >slankka</a>
-      </footer>
-    </template>
+    <PageSub />
   </ParentLayout>
 </template>
 
 <style scoped>
-  .footer {
-    font-size: 12px;
-    padding: .5rem;
-    border-top: 1px solid #eaecef;
-    text-align: center;
-  }
+
 </style>
 
 <script>
 import ParentLayout from "@parent-theme/layouts/Layout.vue";
+import PageSub from "@theme/components/Page.vue";
 export default {
   name: "Layout",
   components: {
     ParentLayout,
+    PageSub,
   },
 };
 </script>
